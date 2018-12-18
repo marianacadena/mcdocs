887e6d9a8ca922c302773401eae4602a96909143
Unknown <Cris_up@outlook.com>
2018-12-18 12:52:15 -0600

    firma electronica

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,7 +17,8 @@ class HomeController < ApplicationController
     @doc_uploaded.academico = current_academico
     if @doc_uploaded.archivo.content_type == "application/pdf"
       @doc_uploaded.formato = "pdf"
-      @doc_uploaded.archivo = FirmaElectronica.new.generar_certificado(params.require(:documento).permit(:archivo).path.to_s, params[:pem_file], params[:pass_pem_upload])
+      pdf_temp = Tempfile.new(params.require(:documento).permit(:archivo))
+      @doc_uploaded.archivo = FirmaElectronica.new.generar_certificado(pdf_temp, params[:pem_file], params[:pass_pem_upload])
     else
       @doc_uploaded.formato = "docx"
     end
